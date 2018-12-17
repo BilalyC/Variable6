a6f3b0a65d2f9af89df34e8b0a256a0b2dde0c16
BilalyC <bilaly.camara@outlook.com>
2018-12-17 17:12:24 +0100

    Exercice 6 Complet

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez saisir votre nom, prénom et année de naissance : ");
+            string nom = Console.ReadLine();
+            string prenom = Console.ReadLine();
+            int annee = int.Parse(Console.ReadLine());
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Fiches de renseignement :");
+            Console.WriteLine("\t - Nom : " + nom);
+            Console.WriteLine("\t - Prénom : " + prenom);
+            Console.WriteLine("\t - Age : " + (int.Parse(date.ToString("yyyy")) - annee));
+        }
+    }
+}
